64c8e12bef99df74375228503c8842f38008cd43
liukenan <liukenan@meetstarinc.com>
2020-11-12 19:10:54 +0800

    后台界面初步搭建,首页基本完成

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import Main from "./pages/Main";
+import reportWebVitals from "./reportWebVitals";
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+ReactDOM.render(<Main />, document.getElementById("root"));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
