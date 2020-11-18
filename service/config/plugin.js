f0816f28339abf06539ff2bddf0264ea2931973f
liukenan <liukenan@meetstarinc.com>
2020-11-18 20:00:58 +0800

    登录页中台,配置路由

---
+++ service/config/plugin.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/** @type Egg.EggPlugin */
+// module.exports = {
+  // had enabled by egg
+  // static: {
+  //   enable: true,
+  // }
+// };
+// 配置组件egg-mysql
+exports.mysql={
+  enable:true,
+  package:'egg-mysql'
+}
+exports.cors={
+  enable:true,
+  package:'egg-cors'
+}
